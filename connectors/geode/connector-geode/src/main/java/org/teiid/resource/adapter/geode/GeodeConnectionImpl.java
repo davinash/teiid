f1562e11599afe440032b7a1249fbf42164e54c4
Avinash <avinash@ampool.io>
2017-08-17 17:46:34 +0530

    Adding connection related code

--- connectors/geode/connector-geode/src/main/java/org/teiid/resource/adapter/geode/GeodeConnectionImpl.java
+++ connectors/geode/connector-geode/src/main/java/org/teiid/resource/adapter/geode/GeodeConnectionImpl.java
@@ -4,42 +4,57 @@
 package org.teiid.resource.adapter.geode;
 
 
-import javax.resource.ResourceException;
-
-import org.teiid.resource.spi.BasicConnection;
-import org.teiid.logging.LogManager;
+import org.apache.geode.cache.client.ClientCacheFactory;
+import org.apache.geode.internal.cache.GemFireCacheImpl;
 import org.teiid.core.BundleUtil;
 import org.teiid.logging.LogConstants;
+import org.teiid.logging.LogManager;
+import org.teiid.resource.spi.BasicConnection;
 import org.teiid.translator.geode.GeodeConnection;
 
+import java.util.Properties;
+import javax.resource.ResourceException;
+
 /**
- * Connection to the resource. You must define geodeConnection interface, that 
+ * Connection to the resource. You must define geodeConnection interface, that
  * extends the "javax.resource.cci.Connection".  If a custom translator is also being created,
  * the Connection interface should be defined in that project or a common project.
  */
 public class GeodeConnectionImpl extends BasicConnection implements GeodeConnection {
 
-	public static final BundleUtil UTIL = BundleUtil.getBundleUtil(GeodeConnectionImpl.class);
+  public static final BundleUtil UTIL = BundleUtil.getBundleUtil(GeodeConnectionImpl.class);
+  GemFireCacheImpl geodeClientCache = null;
 
 
-    private GeodeManagedConnectionFactory config;
+  private GeodeManagedConnectionFactory config;
 
-    public GeodeConnectionImpl(GeodeManagedConnectionFactory env) {
-        this.config = env;
-        // todo: connect to your source here
-        
-        LogManager.logDetail(LogConstants.CTX_CONNECTOR, "geode Connection has been created."); //$NON-NLS-1$
+  public GeodeConnectionImpl(GeodeManagedConnectionFactory env) {
+    this.config = env;
+    // todo: connect to your source here
+    final Properties cacheProperties = new Properties();
+    ClientCacheFactory ccf = new ClientCacheFactory(cacheProperties);
+    this.config.getLocatorAddrPortPairList().forEach(P-> ccf.addPoolLocator(P.getFirst(), P.getSecond()));
+    ccf.setPdxReadSerialized(true);
 
+    this.geodeClientCache = (GemFireCacheImpl) ccf.create();
+
+    LogManager
+        .logDetail(LogConstants.CTX_CONNECTOR, "geode Connection has been created."); //$NON-NLS-1$
+
+  }
+
+
+  @Override
+  public void close() {
+    if ( this.geodeClientCache != null) {
+      this.geodeClientCache.close();
     }
-    
-    @Override
-    public void close() {
-        LogManager.logDetail(LogConstants.CTX_CONNECTOR, "geode Connection has been closed."); //$NON-NLS-1$
-    	
-    }
+    LogManager
+        .logDetail(LogConstants.CTX_CONNECTOR, "geode Connection has been closed."); //$NON-NLS-1$
+  }
 
-    @Override
-    public void someMethod() throws ResourceException {
+  @Override
+  public void someMethod() throws ResourceException {
 
-    }
+  }
 }
--- connectors/geode/connector-geode/src/main/java/org/teiid/resource/adapter/geode/GeodeManagedConnectionFactory.java
+++ connectors/geode/connector-geode/src/main/java/org/teiid/resource/adapter/geode/GeodeManagedConnectionFactory.java
@@ -11,63 +11,118 @@ import org.teiid.resource.spi.BasicManagedConnectionFactory;
 
 import org.teiid.core.BundleUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class GeodeManagedConnectionFactory extends BasicManagedConnectionFactory {
 
-	public static final BundleUtil UTIL = BundleUtil.getBundleUtil(GeodeManagedConnectionFactory.class);
+  public static final BundleUtil
+      UTIL =
+      BundleUtil.getBundleUtil(GeodeManagedConnectionFactory.class);
 
-	private String sampleProperty = null;
-	
-	@Override
-	public BasicConnectionFactory<GeodeConnectionImpl> createConnectionFactory() throws ResourceException {
+  private String sampleProperty = null;
+  private String locatorList;
+  private List<Pair<String, Integer>> locatorAddrPortPairList;
 
-		if (sampleProperty == null) {
-	 		throw new InvalidPropertyException(UTIL.getString("GeodeManagedConnectionFactory.sampleproperty_not_set")); //$NON-NLS-1$
-		}
-		
-		return new BasicConnectionFactory<GeodeConnectionImpl>() {
-			@Override
-			public GeodeConnectionImpl getConnection() throws ResourceException {
-				return new GeodeConnectionImpl(GeodeManagedConnectionFactory.this);
-			}
-		};
-	}	
-	
-	// ra.xml files getters and setters go here.
-	public String getSampleProperty() {
-		return sampleProperty;
-	}
-	
-	public void setSampleProperty(String property) {
-		this.sampleProperty = property;
-	}
-		
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime
-				* result
-				+ ((sampleProperty == null) ? 0 : sampleProperty.hashCode());
-		return result;
-	}
+  @Override
+  public BasicConnectionFactory<GeodeConnectionImpl> createConnectionFactory()
+      throws ResourceException {
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-			
-		GeodeManagedConnectionFactory other = (GeodeManagedConnectionFactory) obj;
+    if (sampleProperty == null) {
+      throw new InvalidPropertyException(
+          UTIL.getString("GeodeManagedConnectionFactory.sampleproperty_not_set")); //$NON-NLS-1$
+    }
 
-		if (!checkEquals(this.getSampleProperty(), other.getSampleProperty())) {
-			return false;
-		}
+    if ( locatorList == null) {
+      throw new InvalidPropertyException(UTIL.getString("GeodeManagedConnectionFactory.locatorList is not set"));
+    }
 
-		return true;
+    validateLocatorProperty();
 
-	}
+    return new BasicConnectionFactory<GeodeConnectionImpl>() {
+      @Override
+      public GeodeConnectionImpl getConnection() throws ResourceException {
+        return new GeodeConnectionImpl(GeodeManagedConnectionFactory.this);
+      }
+    };
+  }
+
+  private void validateLocatorProperty()  throws ResourceException {
+    final String locators = this.getLocatorList();
+    this.locatorAddrPortPairList = new ArrayList<>();
+    String[] locatorSpecs = locators.split(",");
+    for (String locatorSpec : locatorSpecs) {
+      String[] addrPort = locatorSpec.split(Pattern.quote("["));
+      if (addrPort.length != 2) {
+        throw new InvalidPropertyException(
+            "Allowed format for locators is: \"addr[port], addr[port]\"");
+      } else if (addrPort[1].charAt(addrPort[1].length() - 1) != ']') {
+        throw new InvalidPropertyException(
+            "Allowed format for locators is: \"addr[port], addr[port]\"");
+      } else if (addrPort[1].length() < 2) {
+        throw new InvalidPropertyException(
+            "Allowed format for locators is: \"addr[port], addr[port]\"");
+      }
+      String addr = addrPort[0];
+      Integer port = Integer.parseInt(addrPort[1].substring(0, addrPort[1].length() - 1));
+      this.locatorAddrPortPairList.add(new Pair<>(addr, port));
+    }
+  }
+
+  // ra.xml files getters and setters go here.
+  public String getSampleProperty() {
+    return sampleProperty;
+  }
+
+  public void setSampleProperty(String property) {
+    this.sampleProperty = property;
+  }
+
+  public String getLocatorList() { return locatorList;}
+
+  public void setLocatorList(String locatorList) {
+    this.locatorList = locatorList;
+  }
+
+  public List<Pair<String, Integer>> getLocatorAddrPortPairList() {
+    return this.locatorAddrPortPairList;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime
+        * result
+        + ((sampleProperty == null) ? 0 : sampleProperty.hashCode())
+        + ((locatorList == null) ? 0 : locatorList.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+
+    GeodeManagedConnectionFactory other = (GeodeManagedConnectionFactory) obj;
+
+    if (!checkEquals(this.getSampleProperty(), other.getSampleProperty())) {
+      return false;
+    }
+    if (!checkEquals(this.getLocatorList(), other.getLocatorList())) {
+      return false;
+    }
+
+    return true;
+
+  }
 
 }
